3bf7ff9c58fdbc87ca700593f1f93f15643ef835
sin-dorei <228827120@qq.com>
2021-03-19 05:37:39 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/help', 'StaticPageController@help')->name('help');
 Route::get('/about', 'StaticPageController@about')->name('about');
 
 Route::get('/signup', 'UserController@create')->name('signup');
+Route::resource('user', 'UserController');
