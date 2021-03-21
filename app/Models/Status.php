4ef7657d6af24e8e0cee3f55a0c5f0cfb19c1f61
sin-dorei <228827120@qq.com>
2021-03-21 07:55:32 +0000

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -4,9 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Status
+ *
+ * @property int $id
+ * @property string $content
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Status newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Status newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Status query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Status whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Status whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Status whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Status whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Status whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Status extends Model
 {
-    protected $fillable = ['text'];
+    protected $fillable = ['content'];
 
     public function user()
     {
